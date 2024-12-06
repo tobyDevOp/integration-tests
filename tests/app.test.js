fe0afc503c8ab6573d66c2552ac32432dc42cf99
tobygoetz <goetz.tobiah@student.greenriver.edu>
2024-12-05 20:47:37 -0800

    Fixed Breaking Changes for Tests w/Github Actions

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,13 +5,13 @@ describe('Products endpoints', () =>
     {
     test('should be available', async () => {
         const response = await request(app)
-            .get('/api/v1/products')
+            .get('/api/v2/products')
             .expect(200); 
     })
     
     test('should return correct data', async () => {
         const response = await request(app)
-            .get('/api/v1/products')
+            .get('/api/v2/products')
             .expect(200); 
     
         expect(response.body.status).toBe("success");
