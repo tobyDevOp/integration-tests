749cf29af735e92776f7d70989ccc9ff705f331f
tobygoetz <goetz.tobiah@student.greenriver.edu>
2024-12-05 20:23:25 -0800

    Committing Initial Files

---
+++ tests/app.test.js
@@ -0,0 +1,25 @@
+import app from './../src/app.js';
+import request from 'supertest';
+
+describe('Products endpoints', () => 
+    {
+    test('should be available', async () => {
+        const response = await request(app)
+            .get('/api/v1/products')
+            .expect(200); 
+    })
+    
+    test('should return correct data', async () => {
+        const response = await request(app)
+            .get('/api/v1/products')
+            .expect(200); 
+    
+        expect(response.body.status).toBe("success");
+        expect(response.body.data).toEqual([
+            { id: 1, name: "IPhone" },
+            { id: 2, name: "Car" },
+        ]);
+    })
+})
+
+
